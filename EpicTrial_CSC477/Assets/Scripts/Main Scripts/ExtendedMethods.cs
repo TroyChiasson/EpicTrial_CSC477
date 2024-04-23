b968064c4f23138debce4d432b0783e7b55d1f97
JeatonW <49039102+JeatonW@users.noreply.github.com>
2024-04-23 18:29:33 -0500

    added extended methods for local position and local scale

--- EpicTrial_CSC477/Assets/Scripts/Main Scripts/ExtendedMethods.cs
+++ EpicTrial_CSC477/Assets/Scripts/Main Scripts/ExtendedMethods.cs
@@ -9,4 +9,16 @@ public static class ExtendedMethods {
         if (y is not null) { vel.y = y.Value; }
         rb.velocity = vel;
     }
+    public static void SetLocalPos(this Transform tf, float? x = null, float? y = null) {
+        var pos = tf.localPosition;
+        if (x is not null) { pos.x = x.Value; }
+        if (y is not null) { pos.y = y.Value; }
+        tf.localPosition = pos;
+    }
+    public static void SetLocalScale(this Transform tf, float? x = null, float? y = null) {
+        var scale = tf.localScale;
+        if (x is not null) { scale.x = x.Value; }
+        if (y is not null) { scale.y = y.Value; }
+        tf.localScale = scale;
+    }
 }
