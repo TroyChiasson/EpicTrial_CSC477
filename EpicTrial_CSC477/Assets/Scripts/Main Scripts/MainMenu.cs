ad7c1c56721f8f40fb744fc53439fa4e3663a91b
ebm04 <139780145+ebm04@users.noreply.github.com>
2024-04-23 20:14:41 -0500

    Pushing All UI elements to new 3D proj

---
+++ EpicTrial_CSC477/Assets/Scripts/Main Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject SettingsMenu;
+    public GameObject ControlsMenu;
+
+    void Start()
+    {
+        SettingsMenu.SetActive(false);
+        ControlsMenu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }   
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
