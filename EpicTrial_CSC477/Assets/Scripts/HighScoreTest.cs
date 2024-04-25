93ff73736656977f546c0a794fc46ae88859104c
Troy Chiasson <chiassonmtroy@gmail.com>
2024-04-25 18:40:15 -0500

    High Score files and test

---
+++ EpicTrial_CSC477/Assets/Scripts/HighScoreTest.cs
@@ -0,0 +1,28 @@
+using HighScore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreTest : MonoBehaviour
+{
+    private float delay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HS.Init(this, "Troy's Game");
+        delay = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (delay > 0) {
+            delay -= Time.deltaTime;
+            if (delay < 0) {
+                HS.SubmitHighScore(this, "Troy Himself", Random.Range(100,1000));
+            }
+        }
+    }
+
+
+}
