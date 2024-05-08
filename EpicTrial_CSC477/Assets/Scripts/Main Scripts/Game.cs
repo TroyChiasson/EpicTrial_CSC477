e7d2f91b84d95afad4dd86696db388e5df34a4be
ebm04 <139780145+ebm04@users.noreply.github.com>
2024-05-07 20:07:17 -0500

    Working on Scene progression

--- EpicTrial_CSC477/Assets/Scripts/Main Scripts/Game.cs
+++ EpicTrial_CSC477/Assets/Scripts/Main Scripts/Game.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
 
     public static Game Instance { get; private set; }
     public Input input;
+
+    // Fade Objects
+    public Image img; 
 
     // Start is called before the first frame update
     void Start() {
@@ -22,7 +26,8 @@ public class Game : MonoBehaviour {
 
         if (enemies.Length == 0)
         {
-            ProgressToNextScene();
+            StartCoroutine(FadeImage(true));
+            //ProgressToNextScene();
         }
     }
 
@@ -31,5 +36,31 @@ public class Game : MonoBehaviour {
         
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;        
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
     }
 }
