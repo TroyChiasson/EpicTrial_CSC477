f7d710e80535bed435920936906338730be74094
ebm04 <139780145+ebm04@users.noreply.github.com>
2024-05-14 16:37:00 -0500

    Puzzle Room Done

--- EpicTrial_CSC477/Assets/Scripts/Main Scripts/PuzzleRoom.cs
+++ EpicTrial_CSC477/Assets/Scripts/Main Scripts/PuzzleRoom.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PuzzleRoom : MonoBehaviour
 {
+
+    public LaserTurret T1;
+    public LaserTurret T2;
+    public LaserTurret T3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( !T1.GetActive() &&  !T2.GetActive() && !T3.GetActive())
+        {
+            ProgressToNextScene();
+        }
+    }
+
+    void ProgressToNextScene()
+    {
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
